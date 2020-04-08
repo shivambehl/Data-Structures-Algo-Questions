231fbf7fdafb70ba2bbe919f2407802f926ca480
Shivam Behl <shivambehl123@gmail.com>
2020-04-08 23:43:16 +0530

    Add Que

---
+++ DP/WinesProblem.cpp
@@ -0,0 +1,12 @@
+// Everyone loves wine - MAXCOST - CodeChef
+// https://www.codechef.com/MALI2017/problems/MAXCOST
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+
+int main(){
+	
+}
--- DP/minCoins.cpp
+++ DP/minCoins.cpp
@@ -1,19 +1,23 @@
+// Find minimum number of coins required
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int dp[100005];
-
+// topdown
 int minCoins(int coins[], int val, int n){
+	if(n == 0 || value == 0) 
+		return 0;
     int mn = INT_MAX;
     if(dp[val] != -1)
         return dp[val];
     for(int i = 0; i<n; i++){
-        mn = min(minCoins(coins, val - coins[i], n), mn);
+    	if(coins[i] <= val)
+        	mn = min(minCoins(coins, val - coins[i], n), mn);
     }
     dp[val] = mn;
     return mn;
 }
-
 
 
 int main() {
