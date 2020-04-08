2017d1f0808df92cbec6d6338857fa48cb6b221c
Shivam Behl <shivambehl123@gmail.com>
2020-04-09 02:08:35 +0530

    Add Wines Que

--- DP/WinesProblem.cpp
+++ DP/WinesProblem.cpp
@@ -4,9 +4,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ll long long
+
+int profit(int wines[], int i, int j, int y, int dp[100][100]){
+	
+	if(i > j)
+		return 0;
+
+	if(dp[i][j] != -1)
+		return dp[i][j];
+    
+	int op1 = wines[i]*y + profit(wines, i+1, j, y+1, dp);
+	int op2 = wines[j]*y + profit(wines, i, j-1, y+1, dp);
+	dp[i][j] = max(op1, op2);
+	
+	return dp[i][j];
+}
 
 
 int main(){
+	int wines[] = {2, 3, 5, 1, 4};
+	int n = sizeof(wines)/sizeof(int);
+	int dp[100][100];
+    memset(dp, -1, sizeof(dp));
+	cout<<profit(wines, 0, n-1, 1, dp);
 	
+	return 0;
 }
