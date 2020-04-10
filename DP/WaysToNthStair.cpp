04c1142a00cb6905ce21f19e7d6be92ce543b554
Shivam Behl <shivambehl123@gmail.com>
2020-04-10 23:23:37 +0530

    Add String

---
+++ DP/WaysToNthStair.cpp
@@ -0,0 +1,11 @@
+// Count ways to N'th Stair
+// https://practice.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	int m;
+	cout<<m/2 + 1;
+	return 0;
+}
