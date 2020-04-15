be57677876836187e7005b4e77dc7624b21b5576
Shivam Behl <shivambehl123@gmail.com>
2020-04-15 18:55:21 +0530

    Add UniquePaths

---
+++ DP/uniquePaths.cpp
@@ -0,0 +1,30 @@
+// LeetCode - Unique Paths
+// https://leetcode.com/problems/unique-paths/submissions/
+
+// Almost Same as Robots and Paths - CodeChef
+// https://www.codechef.com/problems/CD1IT4
+
+
+#include<iostream>
+using namespace std;
+
+int uniquePaths(int m, int n) {
+    int dp[m][n];
+    for(int i = 0; i<m; i++)
+        dp[i][0] = 1;
+    for(int i = 0; i<n; i++)
+        dp[0][i] = 1;
+    
+    for(int i = 1; i<m; i++){
+        for(int j = 1; j<n; j++){
+            dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
+    }
+    return dp[m-1][n-1];
+}
+
+int main(){
+	int m, n;
+	cin>>m>>n;
+	cout<<uniquePaths(m, n);
+}
