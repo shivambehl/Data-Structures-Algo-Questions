d146655215805ba6415dd83af9484a03fc89d42b
Shivam Behl <shivambehl123@gmail.com>
2020-07-21 02:13:13 +0530

    Add delete nth node from last question

---
+++ Linked List/nthfromend.cpp
@@ -0,0 +1,37 @@
+// Remove Nth node from end
+// Link - https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+// Beautiful use of Dummy Node
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy;
+        dummy.next = head;
+        ListNode* first = &dummy;
+        ListNode* second = &dummy;
+        
+        for(int i = 0; i<n+1; i++){
+            first = first->next;
+        }
+        
+        while(first != NULL){
+            first = first->next;
+            second = second->next;
+        }
+        second->next = second->next->next;
+        
+        return dummy.next;
+    }
+};
